f7cd6dd5b2731e5dc43ca521c84adabac7911849
Zhikh1 <aptem1997zhikh@mail.ru>
2022-10-09 23:26:56 +0300

    parser done

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nomargen <nomargen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 13:32:06 by nomargen          #+#    #+#             */
-/*   Updated: 2022/10/06 23:22:50 by nomargen         ###   ########.fr       */
+/*   Updated: 2022/10/09 23:22:12 by nomargen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../inc/main.h"
@@ -173,12 +173,12 @@ int	parce_ceilling_n_floor_colors(t_game *game, int map_file_fd, int *line)
 			color_ptr = &game->floor_color;
 		else
 			color_ptr = &game->ceilling_color;
-		if (!parse_rgb(color_ptr, &crnt_str[3], *line))
+		if (!parse_rgb(color_ptr, &crnt_str[2], *line))
 			return (safe_free(crnt_str, 0));
 		free(crnt_str);
 		i++;
 	}
-	return (game->map_size_x);
+	return (1);
 }
 
 int	parce_textures(t_game *game, int map_file_fd, int *line)
@@ -210,7 +210,7 @@ int	parce_textures(t_game *game, int map_file_fd, int *line)
 	return (1);
 }
 
-int is_map_char(char ch)
+int	is_map_char(char ch)
 {
 	if (ch == '0' || ch == '1' || ch == ' ')
 		return (1);
@@ -219,16 +219,26 @@ int is_map_char(char ch)
 	return (0);
 }
 
-int	check_map_line_content(char *crnt_str, int line)
+int	check_map_line_content(char *crnt_str, int line, int *player_cnt)
 {
+	int	char_type;
+
 	while (*crnt_str)
 	{
-		if (!is_map_char(*crnt_str))
+		char_type = is_map_char(*crnt_str);
+		if (char_type == 0)
 		{
 			print_err_with_header(line, "Wrong symbol \"");
 			ft_putchar_fd(*crnt_str, ERR_FD);
-			ft_putstr_fd("\" in map!\nSymbols in map can be only:\
+			ft_putstr_fd("\" in map!\nSymbols in map can be only: \
 \"0\",\"1\",\" \",\"N\",\"S\",\"E\",\"W\".\n", ERR_FD);
+			return (0);
+		}
+		if (char_type == 2)
+			(*player_cnt)++;
+		if (*player_cnt > 1)
+		{
+			print_err_with_header(line, "A map can only contain one player!\n");
 			return (0);
 		}
 		crnt_str++;
@@ -236,26 +246,39 @@ int	check_map_line_content(char *crnt_str, int line)
 	return (1);
 }
 
-//доработать отсутствие карты
-//проработать обнаружение игрока, его отсутствие и их избыток
+size_t	check_map_line_len(char *crnt_str, int *line_len, int *line)
+{
+	int	tmp_len;
+
+	tmp_len = ft_strlen(crnt_str);
+	if (crnt_str[tmp_len - 1] == '\n')
+	{
+		crnt_str[tmp_len - 1] = 0;
+		tmp_len--;
+	}
+	if (tmp_len == 0)
+	{
+		print_err_with_header(*line, "Map cannot have empty fields!\n");
+		return (0);
+	}
+	*line_len = tmp_len;
+	return (1);
+}
+
 int	check_map(t_game *game, int map_file_fd, int *line)
 {
 	char	*crnt_str;
 	int		line_len;
+	int		player_cnt;
 
+	player_cnt = 0;
 	crnt_str = get_next_line(map_file_fd);
 	(*line)++;
 	while (crnt_str != NULL)
 	{
-		line_len = ft_strlen(crnt_str);
-		if (crnt_str[line_len - 1] == '\n')
-		{
-			crnt_str[line_len - 1] = 0;
-			line_len--;
-		}
-		if (line_len == 0)
-			return (print_err_with_header(*line, "Map cannot have empty fields!\n"));
-		if (!check_map_line_content(crnt_str, *line))
+		if (!check_map_line_len(crnt_str, &line_len, line))
+			return (safe_free(crnt_str, 0));
+		if (!check_map_line_content(crnt_str, *line, &player_cnt))
 			return (safe_free(crnt_str, 0));
 		if (game->map_size_x < line_len)
 			game->map_size_x = line_len;
@@ -264,7 +287,133 @@ int	check_map(t_game *game, int map_file_fd, int *line)
 		crnt_str = get_next_line(map_file_fd);
 		(*line)++;
 	}
+	if (game->map_size_y == 0)
+		print_err_with_header(*line, "Map cannot be empty!\n");
+	else if (player_cnt == 0)
+		print_err_with_header(*line, "The map must contain one player!\n");
 	return (1);
+}
+
+void	free_alloced_ptr_mas(char *mass[], size_t size)
+{
+	size_t	i;
+
+	if (mass == NULL)
+		return ;
+	i = 0;
+	while (i < size)
+	{
+		free(mass[i]);
+		i++;
+	}
+	free (mass);
+}
+
+int	close_fd_ret(int fd, int ret)
+{
+	close(fd);
+	return (ret);
+}
+
+char	*get_map_line(int map_file_fd, int map_line_size)
+{
+	char	*map_line;
+	char	*map_str;
+	int		map_str_len;
+
+	map_str = get_next_line(map_file_fd);
+	if (map_str == NULL)
+	{
+		ft_putstr_fd("Allocation fail!\n", ERR_FD);
+		return (NULL);
+	}
+	map_line = malloc(sizeof(char) * map_line_size);
+	if (map_line == NULL)
+	{
+		ft_putstr_fd("Allocation fail!\n", ERR_FD);
+		return (NULL);
+	}
+	map_str_len = ft_strlen(map_str);
+	if (map_str[map_str_len - 1] == '\n')
+		map_str_len--;
+	ft_memset(map_line, ' ', map_line_size);
+	ft_memcpy(map_line, map_str, map_str_len);
+	return (map_line);
+}
+
+void	search_player(t_game *game, int str_num)
+{
+	int		i;
+	char	ch;
+
+	i = 0;
+	while (i < game->map_size_x)
+	{
+		ch = game->map[str_num][i];
+		if (is_map_char(ch) == 2)
+		{
+			game->position.x = i;
+			game->position.y = str_num;
+			game->view_direction.x = (ch == 'W') * (-1.0) + (ch == 'E') * (1.0);
+			game->view_direction.y = (ch == 'N') * (-1.0) + (ch == 'S') * (1.0);
+			game->map[str_num][i] = '0';
+		}
+		i++;
+	}
+}
+
+int	skip_lines(int fd, int cnt)
+{
+	int		i;
+	char	buf;
+
+	i = 0;
+	while (i < cnt)
+	{
+		if (read(fd, &buf, 1) < 0)
+		{
+			ft_putstr_fd("File read fail!\n", ERR_FD);
+			return (0);
+		}
+		if (buf == '\n')
+			i++;
+	}
+	return (1);
+}
+
+int	generate_map(t_game *game, char const *map_file_path)
+{
+	int	i;
+	int	map_file_fd;
+
+	i = 0;
+	game->map = malloc(sizeof(char *) * game->map_size_y);
+	if (game->map == NULL)
+	{
+		ft_putstr_fd("Allocation fail!\n", ERR_FD);
+		return (0);
+	}
+	map_file_fd = open(map_file_path, O_RDONLY);
+	if (map_file_fd < 0)
+	{
+		perror(map_file_path);
+		return (0);
+	}
+	if (!skip_lines(map_file_fd, 8))
+		return (close_fd_ret(map_file_fd, 0));
+	while (i < game->map_size_y)
+	{
+		game->map[i] = get_map_line(map_file_fd, game->map_size_x);		
+		if (game->map[i] == NULL)
+		{
+			ft_putstr_fd("Allocation fail!\n", ERR_FD);
+			free_alloced_ptr_mas(game->map, i);
+			return (close_fd_ret(map_file_fd, 0));
+		}
+		search_player(game, i);
+		i++;
+	}	
+	return (close_fd_ret(map_file_fd, 1));
 }
 
 int	parce_map_file(t_game *game, char const *map_file_path)
@@ -282,33 +431,18 @@ int	parce_map_file(t_game *game, char const *map_file_path)
 		return (0);
 	}
 	if (!parce_textures(game, map_file_fd, &line))
-		return (0);
+		return (close_fd_ret(map_file_fd, 0));
 	if (!check_empty_line(map_file_fd, &line))
-		return (0);
+		return (close_fd_ret(map_file_fd, 0));
 	if (!parce_ceilling_n_floor_colors(game, map_file_fd, &line))
-		return (0);
+		return (close_fd_ret(map_file_fd, 0));
 	if (!check_empty_line(map_file_fd, &line))
-		return (0);
+		return (close_fd_ret(map_file_fd, 0));
 	if (!check_map(game, map_file_fd, &line))
-		return (0);
-	// generate_map(game, map_file_path);
-	close(map_file_fd);
-	return (1);
-}
-
-void	free_alloced_ptr_mas(char *mass[])
-{
-	int	i;
-
-	if (mass == NULL)
-		return ;
-	i = 0;
-	while (mass[i] != NULL)
-	{
-		free(mass[i]);
-		i++;
-	}
-	free (mass);
+		return (close_fd_ret(map_file_fd, 0));
+	if (!generate_map(game, map_file_path))
+		return (close_fd_ret(map_file_fd, 0));
+	return (close_fd_ret(map_file_fd, 1));
 }
 
 void	null_game(t_game *game)
@@ -324,6 +458,8 @@ void	null_game(t_game *game)
 		game->textures_path[i] = NULL;
 		i++;
 	}
+	game->map_size_x = 0;
+	game->map_size_y = 0;
 	game->ceilling_color = 0x0;
 	game->floor_color = 0x0;
 	game->position.x = 0.0;
@@ -338,7 +474,7 @@ void	free_game(t_game *game)
 
 	if (game == NULL)
 		return ;
-	free_alloced_ptr_mas(game->map);
+	free_alloced_ptr_mas(game->map, game->map_size_y);
 	i = 0;
 	while (i != TEXTURE_CNT)
 	{
@@ -376,6 +512,26 @@ int	check_params(int argc, char const *argv[])
 	return (1);
 }
 
+
+void	run_game(t_game *game)
+{
+	for (int i = 0; i < game->map_size_y; i++)
+	{
+		for (int j = 0; j < game->map_size_x; j++)
+			printf("%c", game->map[i][j]);
+		printf("\n");
+	}
+	printf("-----------------------------------------\n");
+	printf("map_size_x:%d \n", game->map_size_x);
+	printf("map_size_x:%d \n", game->map_size_y);
+	printf("pos_x:%f \n", game->position.x);
+	printf("pos_y:%f \n", game->position.y);
+	printf("view_x:%f \n", game->view_direction.x);
+	printf("view_y:%f \n", game->view_direction.y);
+	printf("ceilling_color:%08x \n", game->ceilling_color);
+	printf("floor_color:%08x \n", game->floor_color);
+}
+
 int	main(int argc, char const *argv[])
 {
 	t_game	game;
@@ -389,7 +545,7 @@ int	main(int argc, char const *argv[])
 	null_game(&game);
 	if (!parce_map_file(&game, argv[1]))
 		return (1);
-	// run_game(&game);
+	run_game(&game);
 	free_game(&game);
 	return (0);
 }
